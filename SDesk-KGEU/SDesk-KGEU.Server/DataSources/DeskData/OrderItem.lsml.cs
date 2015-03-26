759e16806024341a64d485523c569ef988bc2b09
RustemGimazetdinov <grfbox@mail.ru>
2015-03-26 14:43:08 +0300

    newFeature

--- SDesk-KGEU/SDesk-KGEU.Server/DataSources/DeskData/OrderItem.lsml.cs
+++ SDesk-KGEU/SDesk-KGEU.Server/DataSources/DeskData/OrderItem.lsml.cs
@@ -7,5 +7,17 @@ namespace LightSwitchApplication
 {
     public partial class OrderItem
     {
+        partial void Status_Compute(ref decimal result)
+        {
+            var taskCnt = this.Task.Count();
+            if (taskCnt == 0)
+            {
+                result = 0;
+                return;
+            }
+            var closedCnt = this.Task.Count(t => t.StatusItem.StatusId == 3);
+            result = (decimal) closedCnt / taskCnt;
+            // Присвоение результату значения нужного поля
+        }
     }
 }
