9857597c25ccf3ae3c122152666f0db5c8312577
IlnurV <fiests@gmail.com>
2015-03-28 19:10:29 +0300

    fix

--- SDesk-KGEU/SDesk-KGEU.Server/DataSources/DeskData/OrderItem.lsml.cs
+++ SDesk-KGEU/SDesk-KGEU.Server/DataSources/DeskData/OrderItem.lsml.cs
@@ -7,16 +7,33 @@ namespace LightSwitchApplication
 {
     public partial class OrderItem
     {
-        partial void Status_Compute(ref decimal result)
+       
+
+        partial void Status_Compute(ref string result)
         {
             var taskCnt = this.Task.Count();
             if (taskCnt == 0)
             {
-                result = 0;
+                result = "Новая";
                 return;
             }
-            var closedCnt = this.Task.Count(t => t.StatusItem.StatusId == 3);
-            result = (decimal) closedCnt / taskCnt;
+            //var closedCnt = this.Task.Count(t => t.StatusItem.StatusId == 3);
+            if (this.Task.Any(p=>p.StatusItem.StatusId==2))
+            {
+                result = "В работе" ;
+                return;
+            }
+            else if (this.Task.Any(p=>p.StatusItem.StatusId==5))
+            {
+               result = "Требует уточнения" ;
+                return; 
+            }
+            else if (this.Task.All(p=>p.StatusItem.StatusId==3))
+            {
+               result = "Выполнена" ;
+                return;   
+            }
+            
             // Присвоение результату значения нужного поля
         }
     }
