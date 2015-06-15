1c5bd7780abb3ab6858c6d1159ab56e3edc55ce4
IlnurV <fiests@gmail.com>
2015-06-15 18:41:56 +0300

    refix

---
+++ SDesk-KGEU/SDesk-KGEU.HTMLClient/Screens/AddEditFileItem.lsml.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+
+myapp.AddEditFileItem.Image_render = function (element, contentItem) {
+    // Write code here.
+    createImageUploader(element, contentItem, "max-width: 300px; max-height: 300px");
+};
+
+
+myapp.AddEditFileItem.beforeApplyChanges = function (screen) {
+    var filename = $('input[type=file]').val().replace(/.*(\/|\\)/, '');
+    screen.FileItem.FileName = filename;
+};
---
+++ SDesk-KGEU/SDesk-KGEU.HTMLClient/UserCode/FileItem.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.FileItem.created = function (entity) {
+    // Write code here.
+    entity.FileName = 0;
+  
+    entity.FileId = 0;
+
+   
+};
