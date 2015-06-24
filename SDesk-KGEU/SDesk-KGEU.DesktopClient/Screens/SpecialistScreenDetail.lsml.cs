d70afb0e74b76a9fdbe19c1bbe42d6d98569073e
EvgenyKozlov <20Evgeny18@gmail.com>
2015-06-24 18:45:34 +0300

    fix

--- SDesk-KGEU/SDesk-KGEU.DesktopClient/Screens/SpecialistScreenDetail.lsml.cs
+++ SDesk-KGEU/SDesk-KGEU.DesktopClient/Screens/SpecialistScreenDetail.lsml.cs
@@ -7,18 +7,52 @@ using Microsoft.LightSwitch;
 using Microsoft.LightSwitch.Framework.Client;
 using Microsoft.LightSwitch.Presentation;
 using Microsoft.LightSwitch.Presentation.Extensions;
+using System.Windows.Controls;
 namespace LightSwitchApplication
 {
     public partial class SpecialistScreenDetail
     {
 
-
-
-        partial void CloseTask_Execute()
+        partial void SaveFile2_Execute()
         {
-            Task.SelectedItem.StatusItem = DataWorkspace.DeskData.Status.Where(p => p.StatusId == 3).FirstOrDefault();
-            Task.SelectedItem.DateClose = DateTime.Now;
+            {
+                Microsoft.LightSwitch.Threading.Dispatchers.Main.BeginInvoke(() =>
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+
+                    sfd.Filter = "All files (*.*)|*.*";
+                    sfd.DefaultFileName = FileItem1.SelectedItem.FileName;
+                    sfd.FilterIndex = 2;
+                    if (sfd.ShowDialog() == true)
+                    {
+                        using (FileStream fs = new FileStream(sfd.SafeFileName, FileMode.Create))
+                        {
+                            byte[] buf = new byte[fs.Length];
+                            buf = FileItem1.SelectedItem.Image;
+                            fs.Write(buf, 0, buf.Length);
+                        }
+                    }
+                });
+
+            }
+        }
+
+        partial void AddNewComment_Execute()
+        {
+                Comment.AddNew();
+                this.OpenModalWindow("AddNewCommentModalWnd");
+        }
+
+        partial void SaveNewComment1_Execute()
+        {
             DataWorkspace.DeskData.SaveChanges();
+            this.CloseModalWindow("AddNewCommentModalWnd");
+        }
+
+        partial void CancelComment_Execute()
+        {
+            DataWorkspace.DeskData.Details.DiscardChanges();
+            this.CloseModalWindow("AddNewCommentModalWnd");
         }
     }
 }
